9a8c0bfa1cebf0a6d65ae90514b7ae32dd2033c8
chilimango <maldonadot55@yahoo.com>
2020-09-12 11:42:06 -0700

    add arrays

---
+++ db/reservations.js
@@ -0,0 +1,8 @@
+let resArray = [{
+    customerName: 'Teresa',
+    customerEmail: 'maldonadot55@yahoo.com',
+    customerId: '43433',
+    customerPhone: '56312435'
+}];
+
+module.exports = resArray
---
+++ db/tables.js
@@ -0,0 +1,10 @@
+
+
+let tablesArray = [{
+    customerName: 'Teresa',
+    customerEmail: 'maldonadot55@yahoo.com',
+    customerId: '43433',
+    customerPhone: '56312435'
+}];
+
+module.exports = tablesArray
